718662957485efe8150fffaa9f48dd21ed76bb58
daoptph23912 <daoptph23912@fpt.edu.vn>
2024-07-30 23:08:13 +0700

    updated-again

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = (children) => {
+const ProtectedRoute = ({ children }) => {
   const { loading, isAuthenticated } = useSelector((state) => state.user);
   if (loading === false) {
     if (!isAuthenticated) {
